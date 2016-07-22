a3202efd727a008ed4cd93dba7f673068fea1c78
sarunyou <sarunyou.w@ku.th>
2016-07-22 22:28:48 +0700

    add jade compile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var browserSync = require('browser-sync').create();
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const jade = require('gulp-jade');
 
 gulp.task('sass', function() {
   return gulp.src('app/scss/**/*.sass')
@@ -10,6 +11,17 @@ gulp.task('sass', function() {
       stream: true
     }))
 });
+gulp.task('templates', function() {
+  gulp.src('app/template/**/*.jade')
+  .pipe(jade({
+    pretty: true
+  })
+)
+  .pipe(gulp.dest('app'))
+  .pipe(browserSync.reload({
+    stream: true
+  }))
+})
 gulp.task('browserSync', function() {
   browserSync.init({
     server: {
@@ -20,5 +32,6 @@ gulp.task('browserSync', function() {
 gulp.task('watch',['browserSync', 'sass'], function() {
   gulp.watch('app/scss/**/*.sass', ['sass']);
   gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/template/**/*.jade', ['templates']);
   gulp.watch('app/js/**/*.js', browserSync.reload);
 })
